6c3380b53058acc52ec9da24b458cdf5fcec7909
Elizabeth Brown <55103747+ej2brown@users.noreply.github.com>
2020-06-02 17:14:40 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,23 @@
-require './Game'
-require './Question'
-require './Turn'
-require './Player'
+require './game'
+require './question'
+require './turn'
+require './player'
 
-# prompt
-game = Game.new 
+print "Enter name for Player 1: "
+p1 = Player.new(gets.chomp)
+print "Enter name for Player 2: "
+p2 = Player.new(gets.chomp)
 
+game = Game.new(p1, p2)
+game.nextTurn
+
+while (Game.player1.lives > 0 && Game.player2.lives > 0)
+  puts game.status
+  turn = game.next_turn
+  puts turn.prompt
+  puts turn.result
+end
+
+winner = Game.player1.lives <= 0 ? Game.player1 : Game.player2
+puts  "-----GAME OVER-----"
+puts  "#{winner.name} wins with #{winner.lives} lives remaining!"
